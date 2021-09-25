ff5ea7d3cd253f9849d78d5f499cd85b78cc89cf
HectorFranc0u <halejandrofb@gmail.com>
2021-09-24 20:09:34 -0600

    Add project files.

---
+++ Recetas actividad practica/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recetas_actividad_practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Recetas receta = new Recetas();
+            receta.setRecetaName("pastel");
+            receta.setRecetaPrecio(4);
+            receta.setRecetaTipo("postre");
+            receta.setrecetaingredientes("harina, huevos, leche, azucar");
+
+            MessageBox.Show(receta.getRecetaName() + receta.getrecetatipo() + receta.getrecetaingredientes() + receta.getRecetaPrecio());
+
+            receta.recetaautor = "mama pastelera";
+
+            MessageBox.Show(receta.recetaautor);
+        }
+    }
+}
---
+++ Recetas actividad practica/Recetas.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recetas_actividad_practica
+{
+    class Recetas
+    {
+        //definir miembros de la calse
+        // propiedades - atributos - datos
+
+        private string RecetaName;
+        private int RecetaPrecio;
+        private string Recetatipo;
+        private string RecetaIngredientes;
+
+        public string recetaautor { get; set; }
+
+        //metodos de propiedad
+
+        public void setRecetaName(string RName)
+        {
+            RecetaName = RName;
+        }
+
+        public string getRecetaName()
+        {
+            return RecetaName;
+        }
+
+        public void setRecetaPrecio(int Rprecio)
+        {
+            RecetaPrecio = Rprecio;
+        }
+
+        public int getRecetaPrecio()
+        {
+            return RecetaPrecio;
+        }
+
+        public void setRecetaTipo(string rtipo)
+        {
+            Recetatipo = rtipo;
+        }
+        public string getrecetatipo()
+        {
+            return Recetatipo;
+        }
+
+        public void setrecetaingredientes(string Ringredientes)
+        {
+            RecetaIngredientes = Ringredientes;
+        }
+        public string getrecetaingredientes()
+        {
+            return RecetaIngredientes;
+        }
+    }
+}
